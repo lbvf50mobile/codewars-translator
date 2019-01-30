de211b149a2a7e5067d9ded08f54870619856b6c
Dmitry <lbvf50.mobile@gmail.com>
2019-01-30 17:19:39 +0700

    Add solution and test caces

---
+++ solution.lua
@@ -0,0 +1,7 @@
+kata = {}
+function kata.foo()
+  --your code goes here
+  return "expected"
+end
+
+return kata
---
+++ test_cases.lua
@@ -0,0 +1,7 @@
+-- TODO: Replace examples and use TDD development by writing your own tests
+local solution = require 'solution'
+describe("solution", function()
+  it("test for something", function()
+    assert.are.same("expected", solution.foo())
+  end)
+end)
