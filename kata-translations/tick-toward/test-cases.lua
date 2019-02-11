7922f47e0b5911c7b4d39d0d03e8795bd2aa10d2
Dmitry <lbvf50.mobile@gmail.com>
2019-02-11 20:14:38 +0700

    Add tests for tick toward

--- kata-translations/tick-toward/test-cases.lua
+++ kata-translations/tick-toward/test-cases.lua
@@ -4,5 +4,7 @@ local solution = require 'solution'
 describe("tick_toward", function()
   it("gets medial values between two positive x_y coords", function()
     assert.are.same({{2,2},{3,2},{4,2}}, solution.tick_toward({2,2},{4,2}))
+    assert.are.same({{3,2},{4,3},{5,4},{5,5}}, solution.tick_toward({3,2},{5,5}))
+    assert.are.same({{3,2},{4,3},{5,4},{5,5}}, solution.tick_toward({3,2},{5,5}))
   end)
 end)
