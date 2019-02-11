186b87eac559f0c9dcf8b6c653fcd5e80f0a3823
Dmitry <lbvf50.mobile@gmail.com>
2019-02-11 20:12:57 +0700

    Add solution for tick-toward

--- kata-translations/tick-toward/src/solution.lua
+++ kata-translations/tick-toward/src/solution.lua
@@ -1,7 +1,25 @@
 kata = {}
-function kata.foo()
+function kata.tick_toward(start,target)
   --your code goes here
-  return "tick-toward"
+  local a,b =  start[1], start[2]
+  local x,y = target[1], target[2]
+  if (a == x) and (b == y) then return {start} end
+  if( x >  a) then
+    a = a + 1
+  elseif x > a then
+    a = a - 1
+  end
+  if y > b then
+    b = b + 1
+  elseif y < b then
+    b = b - 1
+  end
+  local answer = {start}
+  local rec = kata.tick_toward({a,b},target)
+  for k,v in pairs(rec) do
+    table.insert(answer,v)
+  end
+  return answer
 end
 
 return kata
--- kata-translations/tick-toward/test-cases.lua
+++ kata-translations/tick-toward/test-cases.lua
@@ -2,7 +2,7 @@
 -- TODO: Replace examples and use TDD development by writing your own tests
 local solution = require 'solution'
 describe("tick_toward", function()
-  it("test for something", function()
-    assert.are.same("tick-toward", solution.foo())
+  it("gets medial values between two positive x_y coords", function()
+    assert.are.same({{2,2},{3,2},{4,2}}, solution.tick_toward({2,2},{4,2}))
   end)
 end)
