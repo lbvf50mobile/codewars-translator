7d5ea528f66e825963e26e4656b28050eb5aac27
Dmitry <lbvf50.mobile@gmail.com>
2019-06-25 19:35:55 +0700

    Add working sample tests

--- kata-translations/reversed-words/sample_test_cases.lua
+++ kata-translations/reversed-words/sample_test_cases.lua
@@ -1,7 +1,6 @@
 local solution = require 'solution'
 describe("Base test", function()
   it("From examples", function()
-    assert.are.same({1,2,3,4,5,6,7,8,9,10}, solution.count_by(1,10))
-    assert.are.same({2,4,6,8,10}, solution.count_by(2,5))
+    assert.are.same('world! hello', solution.reverse_words('hello world!'))
   end)
 end)
--- kata-translations/reversed-words/solution_setup.lua
+++ kata-translations/reversed-words/solution_setup.lua
@@ -1,10 +1,5 @@
 return {
-    count_by = function(step,size)
-      -- Make it green, then make it clean :)
-      if size == 10 then
-        return {1,2,3,4,5,6,7,8,9,10}
-      else
-        return {2,4,6,8,10}
-       end 
+  reverse_words = function(str)
+      return 'world! hello'
     end
   }
