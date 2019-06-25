9f4ccd9302cc4fb538e9d8b403f066768bc4d7cb
Dmitry <lbvf50.mobile@gmail.com>
2019-06-25 19:32:10 +0700

    Add two methods

--- kata-translations/reversed-words/solution.lua
+++ kata-translations/reversed-words/solution.lua
@@ -1,5 +1,17 @@
 return {
   reverse_words = function(str)
+      local reverse =  function(arr)
+        local i, j = 1, #arr
+      
+        while i < j do
+          arr[i], arr[j] = arr[j], arr[i]
+      
+          i = i + 1
+          j = j - 1
+        end
+      end
+      words = {}
+      for word in str:gmatch("%w+") do table.insert(words, word) end
       return 'world! hello'
     end
   }
