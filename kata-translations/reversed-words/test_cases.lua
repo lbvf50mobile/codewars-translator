73926c41caadafc78a06e840dc39367484691c7a
Dmitry <lbvf50.mobile@gmail.com>
2019-06-26 15:12:24 +0700

    Generata random string

--- kata-translations/reversed-words/test_cases.lua
+++ kata-translations/reversed-words/test_cases.lua
@@ -26,11 +26,32 @@ local b = {
       string = string .. delimiter .. list[i]
     end
     return string
-  end
-
+  end,
+  implode = function (list, delimiter)
+    local len = #list
+    if len == 0 then
+      return ""
+    end
+    local string = list[1]
+    for i = 2, len do
+      string = string .. delimiter .. list[i]
+    end
+    return string
+  end,
 }
 b.reverse_words = function(str)
   return b.join(b.reverse(b.split(str)))
+end
+-- math.random(lower, upper) generates integer numbers between lower and upper (both inclusive).
+-- string.char (···) Receives zero or more integers.
+-- string.gsub(name, "^a", "")
+b.random_string = function(n)
+  local res = {}
+  for i=1,n,1 do
+    table.insert(res,string.char(math.random(32,32+95)))
+  end
+  local ans = string.gsub(b.implode(res,''),"%s+"," ")
+  return ans
 end
 
 describe("Static", function()
@@ -47,5 +68,16 @@ describe("Static", function()
     assert.are.same("foobar", b.reverse_words("foobar"))
     assert.are.same("editor kata", b.reverse_words("kata editor"))
     assert.are.same("boat your row row row", b.reverse_words("row row row your boat"))
+  end)
+end)
+
+-- math.random(lower, upper) generates integer numbers between lower and upper (both inclusive).
+-- string.char (···) Receives zero or more integers.
+-- string.gsub(name, "^a", "")
+describe("Auto test", function()
+  it("Generate tests", function()
+    for k=1,40,1 do
+      print(b.random_string(40))
+    end
   end)
 end)
