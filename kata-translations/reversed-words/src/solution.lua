964e5c3599f8d0edc8ba536927d998671fb0f872
Dmitry <lbvf50.mobile@gmail.com>
2019-06-26 14:16:39 +0700

    Reverse words solution grouped way

--- kata-translations/reversed-words/src/solution.lua
+++ kata-translations/reversed-words/src/solution.lua
@@ -1,29 +1,35 @@
+local a = {
+  reverse =  function(arr)
+    local i, j = 1, #arr
+    while i < j do
+      arr[i], arr[j] = arr[j], arr[i]
+  
+      i = i + 1
+      j = j - 1
+    end
+    return arr
+  end,
+  split = function(str)
+    words = {}
+      for word in str:gmatch("[^ ]+") do table.insert(words, word) end
+    return words
+  end,
+  join = function (list)
+    local len = #list
+    local delimiter = " "
+    if len == 0 then
+      return ""
+    end
+    local string = list[1]
+    for i = 2, len do
+      string = string .. delimiter .. list[i]
+    end
+    return string
+  end
+
+}
 return {
   reverse_words = function(str)
-      local reverse =  function(arr)
-        local i, j = 1, #arr
-        while i < j do
-          arr[i], arr[j] = arr[j], arr[i]
-      
-          i = i + 1
-          j = j - 1
-        end
-        return arr
-      end
-      words = {}
-      for word in str:gmatch("[^ ]+") do table.insert(words, word) end
-      words = reverse(words)
-      implode = function (delimiter, list)
-        local len = #list
-        if len == 0 then
-          return ""
-        end
-        local string = list[1]
-        for i = 2, len do
-          string = string .. delimiter .. list[i]
-        end
-        return string
-      end
-      return implode(" ", words)
+      return a.join(a.reverse(a.split(str)))
     end
-  }
+}
--- kata-translations/reversed-words/test_cases.lua
+++ kata-translations/reversed-words/test_cases.lua
@@ -1,10 +1,51 @@
 local solution = require 'solution'
-describe("Tests", function()
+local b = {
+  reverse =  function(arr)
+    local i, j = 1, #arr
+    while i < j do
+      arr[i], arr[j] = arr[j], arr[i]
+  
+      i = i + 1
+      j = j - 1
+    end
+    return arr
+  end,
+  split = function(str)
+    words = {}
+      for word in str:gmatch("[^ ]+") do table.insert(words, word) end
+    return words
+  end,
+  join = function (list)
+    local len = #list
+    local delimiter = " "
+    if len == 0 then
+      return ""
+    end
+    local string = list[1]
+    for i = 2, len do
+      string = string .. delimiter .. list[i]
+    end
+    return string
+  end
+
+}
+b.reverse_words = function(str)
+  return b.join(b.reverse(b.split(str)))
+end
+
+describe("Static", function()
   it("Static", function()
     assert.are.same("world! hello", solution.reverse_words("hello world!"))
     assert.are.same("this like speak doesn't yoda", solution.reverse_words("yoda doesn't speak like this"))
     assert.are.same("foobar", solution.reverse_words("foobar"))
     assert.are.same("editor kata", solution.reverse_words("kata editor"))
     assert.are.same("boat your row row row", solution.reverse_words("row row row your boat"))
+  end)
+  it("Static #2", function()
+    assert.are.same("world! hello", b.reverse_words("hello world!"))
+    assert.are.same("this like speak doesn't yoda", solution.reverse_words("yoda doesn't speak like this"))
+    assert.are.same("foobar", b.reverse_words("foobar"))
+    assert.are.same("editor kata", b.reverse_words("kata editor"))
+    assert.are.same("boat your row row row", b.reverse_words("row row row your boat"))
   end)
 end)
