28db184dbddf314f44984fb6836739cbaab9a60d
Dmitry <lbvf50.mobile@gmail.com>
2019-06-28 12:31:54 +0700

    Add smpl2sln function

--- kata-translations/template/alias.sh
+++ kata-translations/template/alias.sh
@@ -11,6 +11,7 @@ help_list(){
     echo "${red}sample_tests${reset} function. (sample_tests - runs setup on sample)"
     echo "${green}full_test${reset} function. (full_test - runs full test)"
     echo "${green}autot${reset} function. (autot - run auto.rb for traslation)"
+    echo "${green}smpl2sln${reset} function. ( Copy: ${red}[solution_setup.lua, sample_test_cases.lua])${reset}=>${green}[solution.lua, test_cases.lua]${reset})"
 }
 
 # just_tests - runs solution on test
@@ -43,5 +44,14 @@ echo "add ${green}autot${reset} function. (autot - run auto.rb for traslation)"
 autot(){
     echo "Runs ${green}auto.rb${reset}"
     ruby auto.rb
+    help_list
+}
+# smpl2sln - samples to solutions. ([solution_setup.lua, sample_test_cases.lua]=>[solution.lua, test_cases.lua])
+echo "add ${green}smpl2sln${reset} function. (Copy: ${red}[solution_setup.lua, sample_test_cases.lua])${reset}=>${green}[solution.lua, test_cases.lua]${reset})"
+smpl2sln(){
+    echo "Copy ${red}solution_setup.lua${reset} to ${green}solution.lua${reset}"
+    cp -f solution_setup.lua solution.lua
+    echo "Copy ${red}sample_test_cases.lua${reset} to ${green}test_cases.lua${reset}"
+    cp -f sample_test_cases.lua test_cases.lua
     help_list
 }
