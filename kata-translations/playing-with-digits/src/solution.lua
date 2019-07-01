80429af8800fa0d270c5f37e875ac73f316aaa49
Dmitry <lbvf50.mobile@gmail.com>
2019-07-01 16:10:46 +0700

    Playing with digits add solution in tests

--- kata-translations/playing-with-digits/src/solution.lua
+++ kata-translations/playing-with-digits/src/solution.lua
@@ -1,8 +1,13 @@
 return {
   dig_pow = function(n,pow)
       -- Make it green, then make it clean :)
-      if 89 == n then return 1
-      elseif 46288 == n then return 51
+      local answer = 0
+      for digit in string.gmatch(tostring(n), '(%d)') do
+        digit = tonumber(digit)
+        answer = answer + math.pow(digit,pow)
+        pow = pow + 1
+      end
+      if 0 == answer%n then return answer/n
       else return -1 end
     end
   }
--- kata-translations/playing-with-digits/test_cases.lua
+++ kata-translations/playing-with-digits/test_cases.lua
@@ -1,4 +1,15 @@
 local solution = require 'solution'
+local s = function(n,pow)
+  -- Make it green, then make it clean :)
+  local answer = 0
+  for digit in string.gmatch(tostring(n), '(%d)') do
+    digit = tonumber(digit)
+    answer = answer + math.pow(digit,pow)
+    pow = pow + 1
+  end
+  if 0 == answer%n then return answer/n
+  else return -1 end
+end
 describe("Playing with Digits sample test cases", function()
   it("Static tests", function()
     assert.are.same(1, solution.dig_pow(89, 1))
