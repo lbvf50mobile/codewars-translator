3c7927773c1645e10a6e954f113357c72acdc9d7
Dmitry <lbvf50.mobile@gmail.com>
2019-07-09 09:52:33 +0700

    Teplate use regex as a global variable.

--- kata-translations/template/auto.rb
+++ kata-translations/template/auto.rb
@@ -5,11 +5,11 @@ Test.assertEquals(shapeArea(1),1)
 Test.assertEquals(shapeArea(5),41)
 FOO
 
-
+@regex = /.*(\d+)\)\,(\d+).*/
 def auto x
     x.split("\n")
     .map{|x| 
-        r = /.*(\d+)\)\,(\d+).*/
+        r = @regex
         x.gsub(r){ "assert.are.same(#{$2}, solution.shape_area(#{$1}))" }
     }
     .join("\n") + "\n"
@@ -17,7 +17,7 @@ end
 def auto_ruby x
     x.split("\n")
     .map{|x| 
-        r = /.*(\[.*\]).*(true|false).*/
+        r = @regex
         x.gsub(r){ "Test.assert_equals(validate_sequence(#{$1}), #{$2})" }
     }
     .join("\n") + "\n"
@@ -26,7 +26,7 @@ end
 
 
 
-# puts auto(input)
+# puts auto_ruby(input)
 puts auto(input)
 
 # https://www.codewars.com/kumite/5d12e46e0fb98b00182f644f/edit
